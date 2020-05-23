195d56bb93b6df4e46a78ee69df279bdbe2b532f
Mika Bessel <mikabessel@googlemail.com>
2020-05-23 21:42:24 +0200

    Config updated

--- config.h
+++ config.h
@@ -8,5 +8,10 @@ const int CARD_DECKS = 8;
 
 const double DECK_PENETRATION = 0.5;
 const double WIDGET_SIZE_MULTIPLICATOR = 1.0;
+const double BLACKJACK_PAYOUT = 1.5;
+
+const bool HIT_SOFT_17 = true;
+const bool DOUBLE_DOWN_AFTER_SPLIT = true;
+const bool HIT_SPLIT_ACES = false;
 
 #endif // CONFIG_H
